bccf0aba9b565ee6e334e164aa8ae18f9a48a714
Olesya1994 <olesia.liakhor@gmail.com>
2023-03-12 04:20:45 +0300

    Initial commit

---
+++ src/task5/BookPair.java
@@ -0,0 +1,19 @@
+package task5;
+
+public class BookPair {
+    private String name;
+    private String autor;
+
+    public BookPair(String name, String autor) {
+        this.name = name;
+        this.autor = autor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+}
---
+++ src/task5/Main.java
@@ -0,0 +1,10 @@
+package task5;
+
+public class Main {
+    static BookPair[] bookPairs = new BookPair[3];
+    public static void main(String[] args) {
+        bookPairs[0] = new BookPair("Анна каренина", "Л.Н. Толстой");
+        bookPairs[0] = new BookPair("Мастер и Маргарита", "М.А. Булгаков");
+        bookPairs[0] = new BookPair("Преступление и Наказание", "Ф.М. Достоевский");
+    }
+}
